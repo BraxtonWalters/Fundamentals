851412d72871c1009bc345ced53ec572408f430d
Braxton Walters <braxton.walters@gmail.com>
2023-07-29 16:31:50 -0400

    init commit

---
+++ test.js
@@ -0,0 +1,29 @@
+const beatles = ['Paul', 'George', 'John', 'Ringo'];
+function printNames(names) {
+    for (let index = 0; index < names.length; index++) {
+      const name = names[index];
+  
+      console.log(name + ' was found at index ' + index);
+    }
+}
+
+
+var foo = "bar";
+function magic(){
+    foo = "hello world";
+    console.log(foo);
+    var foo;
+}
+
+
+magicalUnicorns();
+function magicalUnicorns(){
+    console.log("Will it blend?");
+}
+console.log("Don't breathe this!");
+
+
+
+
+
+
